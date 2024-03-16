cdcbf991c8832b59f744473de42c2370d61b77fe
unknown <christianvallejos74@yahoo.com>
2024-03-15 20:05:24 -0400

    made changes

---
+++ index.js
@@ -0,0 +1,77 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const logo = require("asciiart-logo");
+require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "employees_db",
+  },
+  console.log(`Connected to employees_db database.`)
+);
+function startapp(){
+    const logoText = logo({ name: "employee information \n :p" }).render();
+    console.log(logoText);
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "openingMessage",
+          message: "What would you like to do?",
+          choices: [
+            "viewAllEmployees",
+            "viewAllDepartments",
+            "viewAllRoles",
+            "addADepartment",
+            "addARole",
+            "addAEmployee",
+            "updateEmployee",          
+            "quit",
+          ],
+        },
+      ]).then((inquirerResponse) => {
+        console.log("user selected:    " + inquirerResponse.openingMessage);
+        let choices = inquirerResponse.openingMessage;
+        switch (choices) {
+          case "viewAllEmployees":
+            viewAllEmployees();
+            break;
+          case "viewAllDepartments":
+            viewAllDepartments();
+            break;
+          case "viewAllRoles":
+            viewAllRoles();
+            break;
+          case "addADepartment":
+            addADepartment();
+            break;
+          case "addARole":
+            addARole();
+            break;
+          case "addAEmployee":
+            addAEmployee();
+            break;
+          case "updateEmployee":
+            updateEmployee();
+            break;       
+          case "quit":
+            quit();
+            break;
+          default:
+            console.log("somethings wrong with you");
+            break;
+        }
+      });
+
+
+}
+function viewAllDepartments(){
+    console.log("view department logic")
+    db.query("SELECT id AS department_id, dept_name AS department_name FROM department_list; ", function (err, results) {
+        err ? console.log(err) : console.table(results), startapp()
+    })
+}
+startapp ()
